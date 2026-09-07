4d2a70fbfed4a549dde9c53f1870b8eb1e06be33
alexandre00007 <58002843+alexandre00007@users.noreply.github.com>
2026-09-07 19:42:52 +0100

    fix: deduplicate listings before URL migration

--- tracker/migrations/0003_alter_pricelisting_url.py
+++ tracker/migrations/0003_alter_pricelisting_url.py
@@ -3,6 +3,26 @@
 from django.db import migrations, models
 
 
+def deduplicate_price_listings(apps, schema_editor):
+    PriceListing = apps.get_model('tracker', 'PriceListing')
+    duplicate_keys = (
+        PriceListing.objects.values('product_id', 'retailer_id', 'url')
+        .annotate(count=models.Count('id'))
+        .filter(count__gt=1)
+    )
+
+    for duplicate_key in duplicate_keys:
+        rows = list(
+            PriceListing.objects.filter(
+                product_id=duplicate_key['product_id'],
+                retailer_id=duplicate_key['retailer_id'],
+                url=duplicate_key['url'],
+            ).order_by('price', 'id')
+        )
+        for row in rows[1:]:
+            row.delete()
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -10,6 +30,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.RunPython(deduplicate_price_listings, migrations.RunPython.noop),
         migrations.AlterField(
             model_name='pricelisting',
             name='url',
