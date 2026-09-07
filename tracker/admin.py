001b3ff746661fe3a4469fb48306697ef9cd00d1
alexandre00007 <58002843+alexandre00007@users.noreply.github.com>
2026-09-07 16:47:53 +0100

    chore: expose price history in admin

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from tracker.models import Product, Retailer, PriceListing
+from tracker.models import Product, Retailer, PriceListing, PriceHistory
 
 
-# Register your models here.
 admin.site.register(Product)
 admin.site.register(Retailer)
 admin.site.register(PriceListing)
+admin.site.register(PriceHistory)
