9b5a9df65efbb56d5a76ec374f46ea93614be0cd
alexandre00007 <58002843+alexandre00007@users.noreply.github.com>
2026-09-15 18:48:19 +0100

    feat: expose SearchRun realtime status endpoint

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import scrape_view
+
+from .views import scrape_view, search_run_status
 
 urlpatterns = [
     path('', scrape_view, name='scrape_view'),
+    path('api/search-runs/<uuid:run_id>/status/', search_run_status, name='search_run_status'),
 ]
